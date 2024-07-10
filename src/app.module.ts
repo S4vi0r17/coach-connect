6ddc514a13f7e73e63ca8e26c73b1a103578d452
S4vi0r <gbp17@outlook.com>
2024-07-10 16:02:26 -0500

    chore: Update npm dependencies and add new modules for trainers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TrainerModule } from './trainer/trainer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
+    TrainerModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
